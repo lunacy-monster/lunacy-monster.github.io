d6639b2e5eff1bdfe0d20594793df5a7c9559324
lunacy <101663840+lunacy-monster@users.noreply.github.com>
2022-08-22 23:20:39 +0200

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -1,9 +1,17 @@
 import { PasteClient, Publicity, ExpireDate } from "pastebin-api";
-const client = new PasteClient("DEV_KEY_HERE");
+const client = new PasteClient("DtCde2097z73sYcc8xW3M3_JLv-HLDPZ");
 
 var intext = document.getElementById('text').value;
 document.getElementById('text').value = 'new value';
 
 function upload () {
-    
+    const url = await client.createPaste({
+        code: document.getElementById('text').value = 'new value',
+        expireDate: ExpireDate.Never,
+        format: "javascript",
+        name: "> monokai document",
+        publicity: Publicity.Public,
+      });
+      
+      console.log(url);
 }
