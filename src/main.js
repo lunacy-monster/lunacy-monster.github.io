22a43f5a697384808a529b79b8a9f9d5ca3a12aa
lunacy <101663840+lunacy-monster@users.noreply.github.com>
2022-08-22 22:05:59 +0200

    hm

---
+++ src/main.js
@@ -0,0 +1,36 @@
+import { PasteClient, Publicity, ExpireDate } from "pastebin-api";
+const client = new PasteClient("DEV_KEY_HERE");
+
+var observe;
+if (window.attachEvent) {
+    observe = function (element, event, handler) {
+        element.attachEvent('on'+event, handler);
+    };
+}
+else {
+    observe = function (element, event, handler) {
+        element.addEventListener(event, handler, false);
+    };
+}
+function init () {
+    var text = document.getElementById('text');
+    function resize () {
+        text.style.height = 'auto';
+        text.style.height = text.scrollHeight+'px';
+    }
+
+    function delayedResize () {
+        window.setTimeout(resize, 0);
+    }
+    observe(text, 'change',  resize);
+    observe(text, 'cut',     delayedResize);
+    observe(text, 'paste',   delayedResize);
+    observe(text, 'drop',    delayedResize);
+    observe(text, 'keydown', delayedResize);
+
+    text.focus();
+    text.select();
+    resize();
+}
+var text = document.getElementById('text').value;
+document.getElementById('text').value = 'new value';
